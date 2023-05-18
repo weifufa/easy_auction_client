b27000efba5fc74fb22a89ec771f09f97800cf96
韦ff <1461531243@qq.com>
2023-05-18 19:51:55 +0800

    打包测试

--- config/index.js
+++ config/index.js
@@ -60,6 +60,15 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
+    proxyTable: {
+      '/api': { // 匹配所有以 '/api'开头的请求路径
+        target: 'http://www.weifufa.cn:99', // 代理目标的基础路径
+        changeOrigin: true, // 支持跨域
+        // pathRewrite: {// 重写路径: 去掉路径中开头的'/api'
+        //   '^/api': ''
+        // }
+      }
+    },
 
     /**
      * Source Maps
