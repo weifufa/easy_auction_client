cf48e555e191328394a6d673620a9fc7e055fed6
韦ff <1461531243@qq.com>
2023-05-18 20:58:57 +0800

    重新打包

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -284,7 +284,7 @@ export default {
       this.dataForm.uuid = uuid;
       await reqAptcha(uuid); //发送请求
       this.captchaPath =
-        "http://weifufa:8081/api/member/captcha.jpg?uuid=" + uuid;
+        "http://www.weifufa.cn:99/api/member/captcha.jpg?uuid=" + uuid;
     },
   },
 };
